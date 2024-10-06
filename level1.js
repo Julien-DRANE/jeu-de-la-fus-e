85201ee6ab6b12fffdf02068f9684b9202997719
Julien-DRANE <183376865+Julien-DRANE@users.noreply.github.com>
2024-10-06 20:36:30 +0400

    Update level1.js

--- level1.js
+++ level1.js
@@ -1,25 +1,13 @@
-
-// level1.js
-let elapsedTimeLevel1 = 0;
-
 function gameLoopLevel1() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
     moveRocket();
     updateStars();
     updateObstacles();
-
     drawStars();
     drawObstacles();
     drawRocket();
-
-    elapsedTimeLevel1 += 0.016; 
-
-    if (elapsedTimeLevel1 >= 140) {
-        switchToLevel2();
-        return;
-    }
-
+    elapsedTimeLevel1 += 0.016;
+    if (elapsedTimeLevel1 >= 140) { switchToLevel2(); return; }
     requestAnimationFrame(gameLoopLevel1);
 }
 
