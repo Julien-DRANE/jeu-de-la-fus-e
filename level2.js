fff5c9c899ac0e3a7111b5f12182e74c3b3ae425
Julien-DRANE <183376865+Julien-DRANE@users.noreply.github.com>
2024-10-06 20:54:24 +0400

    Update level2.js

--- level2.js
+++ level2.js
@@ -1,9 +1,4 @@
-const level2Music = new Audio('musique2.mp3');
-level2Music.volume = 1.0;
-let currentPlanet = null;
-const planets = [venusImage, marsImage, mercuryImage];
-
-function generatePlanetLevel2() { /*...*/ }// level2.js
+// level2.js
 const level2Music = new Audio('musique2.mp3');
 level2Music.volume = 1.0;
 let currentPlanet = null;
@@ -133,16 +128,5 @@ function startLevel2() {
     gameLoopLevel2();
     level2Music.play();
 }
-
-startLevel2();
-
-function updatePlanetLevel2() { /*...*/ }
-function drawPlanetLevel2() { /*...*/ }
-function generateStarsLevel2() { /*...*/ }
-function drawStarsLevel2() { /*...*/ }
-function updateObstaclesLevel2() { /*...*/ }
-
-function gameLoopLevel2() { /*...*/ }
-function startLevel2() { /*...*/ }
 
 startLevel2();
