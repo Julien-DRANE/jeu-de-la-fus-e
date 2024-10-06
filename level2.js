51e82c35cd7e47475488809ef1f45d1c43ab9c6a
Julien-DRANE <183376865+Julien-DRANE@users.noreply.github.com>
2024-10-06 19:22:44 +0400

    Update level2.js

--- level2.js
+++ level2.js
@@ -1,8 +1,6 @@
-
 // level2.js
 // Logique spécifique pour le niveau 2
 
-// Charger la musique spécifique au niveau 2
 const level2Music = new Audio('musique2.mp3');
 level2Music.volume = 1.0;
 
@@ -45,7 +43,7 @@ function drawPlanetLevel2() {
     }
 }
 
-// Générer des étoiles rouges vifs et grises pour le niveau 2
+// Générer des étoiles pour le niveau 2
 function generateStarsLevel2() {
     stars = [];
     for (let i = 0; i < numberOfStars; i++) {
@@ -58,7 +56,7 @@ function generateStarsLevel2() {
     }
 }
 
-// Dessiner les étoiles avec des couleurs variées dans le niveau 2
+// Dessiner les étoiles dans le niveau 2
 function drawStarsLevel2() {
     stars.forEach(star => {
         ctx.beginPath();
@@ -69,39 +67,11 @@ function drawStarsLevel2() {
     });
 }
 
-// Mettre à jour la position des bols de soupe avec une trajectoire ondulée
-function updateSoupObstacle(obstacle) {
-    const amplitude = 20 * scaleFactor;
-    const frequency = 0.05;
-    obstacle.x += amplitude * Math.sin(obstacle.y * frequency);
-}
-
-// Générer les obstacles spécifiques au niveau 2
-function generateObstacleLevel2() {
-    const size = (Math.random() * 50 + 30) * scaleFactor;
-    const x = Math.random() * (canvas.width - size);
-    const speed = (Math.random() * 3 + 2) * obstacleSpeedMultiplier * scaleFactor;
-    const imageIndex = Math.floor(Math.random() * level2ObstacleImages.length);
-    const image = level2ObstacleImages[imageIndex];
-    const obstacle = { x, y: -size, size, speed, image };
-
-    // Si l'obstacle est un bol de soupe, ajouter une trajectoire ondulée
-    if (image.src.includes("soupe.png")) {
-        obstacle.isSoup = true;
-    }
-
-    obstacles.push(obstacle);
-}
-
 // Mettre à jour les obstacles du niveau 2
 function updateObstaclesLevel2() {
     for (let i = obstacles.length - 1; i >= 0; i--) {
         let obstacle = obstacles[i];
         obstacle.y += obstacle.speed;
-
-        if (obstacle.isSoup) {
-            updateSoupObstacle(obstacle);
-        }
 
         if (obstacle.y > canvas.height) {
             obstacles.splice(i, 1);
@@ -142,7 +112,6 @@ function gameLoopLevel2() {
 
 // Fonction pour démarrer le niveau 2
 function startLevel2() {
-    // Initialiser les variables spécifiques au niveau 2
     rocket = { ...initialRocket };
     generateStarsLevel2();
     gameLoopLevel2();
